d33b79509318dd356021c6ebba7a50adc420c428
Sharanya <datrangesharanya@gmail.com>
2024-02-22 17:16:30 +0530

    Removed switch case

--- Assignments/Asg3.js
+++ Assignments/Asg3.js
@@ -4,10 +4,14 @@ let arraySize=25;
 
 let arrayAge= [];
 
-let input = prompt('Enter your choice: ');
+// 1. Declare a variable let age = 25;. Write a series of if else statements that will:
 
-switch(parseInt(input)) {
-    case 1: 
+//     Print child to the console if age is less than equal to 12.
+
+//     Print teen to the console if age is between 13 and 18 (inclusive).
+
+//     Print adult to the console if age is above 18
+
         if (age<=12) {
             console.log('child');
         } else if(age >=13 && age <= 18) {
@@ -15,8 +19,8 @@ switch(parseInt(input)) {
         } else {
             console.log('adult');
         }
-        break;
-    case 2:
+
+//2.  Do the same using switch case.
         switch(true) {
             case age<=12:
                 console.log('child');
@@ -27,23 +31,18 @@ switch(parseInt(input)) {
             default:
                 console.log('adult');
         }
-        break;
-    case 3 :
+//3. Declare a variable let arraySize = 25;. Using a for loop, add numbers from one onwards into an array till the arraySize is reached.
+
         for(let i=1;i<=arraySize;i++) {
             arrayAge.push(i);
         }
         console.log(arrayAge);
-        break;
-    case 4:
+
+//4. Accomplish the same using a while loop.
         let i=1;
         while(i <= arraySize) {
             arrayAge.push(i);
             i++;
         }
         console.log(arrayAge);
-        break;
-    default:
-        console.log("Wrong choice ");
-        
-    
-}
+
