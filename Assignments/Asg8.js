7bfdffcf24c9c2398560b78ae081c268648cbab2
Sharanya <datrangesharanya@gmail.com>
2024-02-23 16:36:59 +0530

    modified file Asg8

--- Assignments/Asg8.js
+++ Assignments/Asg8.js
@@ -1,4 +1,12 @@
 //Question 1
+/* Assignments
+1. Write a function that can stop execution of a function for the number of milliseconds sent as an argument
+Example:
+ const func = async () => {
+  console.log(“Printing before”)
+  //Call your function here eg. sleep(3000)
+  console.log(“Printing after”)
+}*/
 function sleep(time) {
     return new Promise ((resolve) => setTimeout(resolve,time));
 }
@@ -15,8 +23,10 @@ const func = async () => {
 }
 
 func();
+// op - Printing before
 
 //Question 2
+/* 2.  Using promises - write a function that fetches data from an API endpoint (GET https://reqres.in/api/users ). Log the data into the console once it is received*/
 function fetchData() {
     return new Promise((resolve, reject ) => {
         const url ='https://reqres.in/api/users';
@@ -50,38 +60,61 @@ fetchData()
         console.error("Error fetchong data: ", error);
     })
 
-//Question 3
+//op - 
 
-// const testAsyncFunction = () =>{
-//     return new Promise((resolve, reject) =>{
-//     if (10> 0.5) {
-//     resolve('Test Resolve');
-//     } else {
-//     reject('Test Reject');
-//     }
-//     }).catch((err) =>{
-//     console.log('Error caught in testAsyncFunction: ', err);
-//     });
-//     };
-// testAsyncFunction()
-// .then((res) =>{
-// console.log('Response in then block: ', res);
-// })
-// .catch((err) => console.log('Error in catch block: ', err));
-
-
-//Question 4
-// const testAsyncFunction = () =>{
-//     return Promise.reject('Test static reject');
-//     };
-// testAsyncFunction()
-// .then((res) =>{
-// console.log('Response in then block', res);
-// })
-// .catch((err) => console.log('Error in catch block', err));
-
-//Question 5 
+//Question 3 -
+/* 
+3. What will be printed to the console when the promise resolves and when it rejects?
 const testAsyncFunction = () =>{
+return new Promise((resolve, reject) =>{
+if (Math.random() > 0.5) {
+resolve('Test Resolve');
+} else {
+reject('Test Reject');
+}
+}).catch((err) =>{
+console.log('Error caught in testAsyncFunction: ', err);
+});
+};
+testAsyncFunction()
+.then((res) =>{
+console.log('Response in then block: ', res);
+})
+.catch((err) => console.log('Error in catch block: ', err));
+
+
+*/
+
+const testAsyncFunction1 = () =>{
+    return new Promise((resolve, reject) =>{
+    if (10> 0.5) {
+    resolve('Test Resolve');
+    } else {
+    reject('Test Reject');
+    }
+    }).catch((err) =>{
+    console.log('Error caught in testAsyncFunction: ', err);
+    });
+    };
+testAsyncFunction1()
+.then((res) =>{
+console.log('Response in then block: ', res);
+})
+.catch((err) => console.log('Error in catch block: ', err));
+
+
+//Question 4 - what will be printed - 
+const testAsyncFunction2 = () =>{
+    return Promise.reject('Test static reject');
+    };
+testAsyncFunction2()
+.then((res) =>{
+console.log('Response in then block', res);
+})
+.catch((err) => console.log('Error in catch block', err));
+
+//Question 5 - what will be printed
+const testAsyncFunction3 = () =>{
     return new Promise((resolve, reject) =>{
     if (Math.random() > 0.5) {
     resolve('Test Resolve');
@@ -93,7 +126,7 @@ const testAsyncFunction = () =>{
     throw new Error('Forced error');
     });
     };
-testAsyncFunction()
+testAsyncFunction3()
 .then((res) =>{
 console.log('Response in then block: ', res);
 })
@@ -101,6 +134,8 @@ console.log('Response in then block: ', res);
 
 
 //Question 6 
+/*
+6. Create a promise that makes a fetch call, but resolves with the data only 2 seconds after the data has been received in the fetch.*/
 function fetchData() {
     return new Promise((resolve, reject ) => {
         const url ='https://reqres.in/api/users';
@@ -126,7 +161,7 @@ function fetchData() {
 
 fetchData() 
 
-//Question 7
+//Question 7 - Complete the above tasks with async/await.
 async function fetchDataAsync() {
     try {
         const response = await fetch('https://reqres.in/api/users');
