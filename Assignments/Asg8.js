ba02077d2f12bf48b27b370fa7c1d1b98ca1304f
Sharanya <datrangesharanya@gmail.com>
2024-02-27 13:07:10 +0530

    Added outputs in comment

--- Assignments/Asg8.js
+++ Assignments/Asg8.js
@@ -23,8 +23,9 @@ const func = async () => {
 }
 
 func();
-// op - Printing before
+// output - Printing before
 
+console.log('2-----------------------------------------------------------------');
 //Question 2
 /* 2.  Using promises - write a function that fetches data from an API endpoint (GET https://reqres.in/api/users ). Log the data into the console once it is received*/
 function fetchData() {
@@ -60,7 +61,61 @@ fetchData()
         console.error("Error fetchong data: ", error);
     })
 
-//op - 
+//output - Data received:  {
+//   page: 1,
+//   per_page: 6,
+//   total: 12,
+//   total_pages: 2,
+//   data: [
+//     {
+//       id: 1,
+//       email: 'george.bluth@reqres.in',
+//       first_name: 'George',
+//       last_name: 'Bluth',
+//       avatar: 'https://reqres.in/img/faces/1-image.jpg'
+//     },
+//     {
+//       id: 2,
+//       email: 'janet.weaver@reqres.in',
+//       first_name: 'Janet',
+//       last_name: 'Weaver',
+//       avatar: 'https://reqres.in/img/faces/2-image.jpg'
+//     },
+//     {
+//       id: 3,
+//       email: 'emma.wong@reqres.in',
+//       first_name: 'Emma',
+//       last_name: 'Wong',
+//       avatar: 'https://reqres.in/img/faces/3-image.jpg'
+//     },
+//     {
+//       id: 4,
+//       email: 'eve.holt@reqres.in',
+//       first_name: 'Eve',
+//       last_name: 'Holt',
+//       avatar: 'https://reqres.in/img/faces/4-image.jpg'
+//     },
+//     {
+//       id: 5,
+//       email: 'charles.morris@reqres.in',
+//       first_name: 'Charles',
+//       last_name: 'Morris',
+//       avatar: 'https://reqres.in/img/faces/5-image.jpg'
+//     },
+//     {
+//       id: 6,
+//       email: 'tracey.ramos@reqres.in',
+//       first_name: 'Tracey',
+//       last_name: 'Ramos',
+//       avatar: 'https://reqres.in/img/faces/6-image.jpg'
+//     }
+//   ],
+//   support: {
+//     url: 'https://reqres.in/#support-heading',
+//     text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
+//   }
+// }
+console.log('3-----------------------------------------------------------------');
 
 //Question 3 -
 /* 
@@ -102,6 +157,62 @@ console.log('Response in then block: ', res);
 })
 .catch((err) => console.log('Error in catch block: ', err));
 
+//output - 
+// Data received:  {
+//     page: 1,
+//     per_page: 6,
+//     total: 12,
+//     total_pages: 2,
+//     data: [
+//       {
+//         id: 1,
+//         email: 'george.bluth@reqres.in',
+//         first_name: 'George',
+//         last_name: 'Bluth',
+//         avatar: 'https://reqres.in/img/faces/1-image.jpg'
+//       },
+//       {
+//         id: 2,
+//         email: 'janet.weaver@reqres.in',
+//         first_name: 'Janet',
+//         last_name: 'Weaver',
+//         avatar: 'https://reqres.in/img/faces/2-image.jpg'
+//       },
+//       {
+//         id: 3,
+//         email: 'emma.wong@reqres.in',
+//         first_name: 'Emma',
+//         last_name: 'Wong',
+//         avatar: 'https://reqres.in/img/faces/3-image.jpg'
+//       },
+//       {
+//         id: 4,
+//         email: 'eve.holt@reqres.in',
+//         first_name: 'Eve',
+//         last_name: 'Holt',
+//         avatar: 'https://reqres.in/img/faces/4-image.jpg'
+//       },
+//       {
+//         id: 5,
+//         email: 'charles.morris@reqres.in',
+//         first_name: 'Charles',
+//         last_name: 'Morris',
+//         avatar: 'https://reqres.in/img/faces/5-image.jpg'
+//       },
+//       {
+//         id: 6,
+//         email: 'tracey.ramos@reqres.in',
+//         first_name: 'Tracey',
+//         last_name: 'Ramos',
+//         avatar: 'https://reqres.in/img/faces/6-image.jpg'
+//       }
+//     ],
+//     support: {
+//       url: 'https://reqres.in/#support-heading',
+//       text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
+//     }
+//   }
+console.log('4-----------------------------------------------------------------');
 
 //Question 4 - what will be printed - 
 const testAsyncFunction2 = () =>{
@@ -112,6 +223,10 @@ testAsyncFunction2()
 console.log('Response in then block', res);
 })
 .catch((err) => console.log('Error in catch block', err));
+//output - Error in catch block Test static reject
+
+
+console.log('5-----------------------------------------------------------------');
 
 //Question 5 - what will be printed
 const testAsyncFunction3 = () =>{
@@ -131,7 +246,10 @@ testAsyncFunction3()
 console.log('Response in then block: ', res);
 })
 .catch((err) => console.log('Error in catch block: ', err));
+//output - Response in then block:  Test Resolve
 
+
+console.log('6-----------------------------------------------------------------');
 
 //Question 6 
 /*
@@ -161,6 +279,8 @@ function fetchData() {
 
 fetchData() 
 
+console.log('7-----------------------------------------------------------------');
+
 //Question 7 - Complete the above tasks with async/await.
 async function fetchDataAsync() {
     try {
@@ -188,3 +308,58 @@ async function fetchDataAndLog() {
 }
 fetchDataAndLog() ;
 
+//output - 
+// Data recieved after delay  {
+//     page: 1,
+//     per_page: 6,
+//     total: 12,
+//     total_pages: 2,
+//     data: [
+//       {
+//         id: 1,
+//         email: 'george.bluth@reqres.in',
+//         first_name: 'George',
+//         last_name: 'Bluth',
+//         avatar: 'https://reqres.in/img/faces/1-image.jpg'
+//       },
+//       {
+//         id: 2,
+//         email: 'janet.weaver@reqres.in',
+//         first_name: 'Janet',
+//         last_name: 'Weaver',
+//         avatar: 'https://reqres.in/img/faces/2-image.jpg'
+//       },
+//       {
+//         id: 3,
+//         email: 'emma.wong@reqres.in',
+//         first_name: 'Emma',
+//         last_name: 'Wong',
+//         avatar: 'https://reqres.in/img/faces/3-image.jpg'
+//       },
+//       {
+//         id: 4,
+//         email: 'eve.holt@reqres.in',
+//         first_name: 'Eve',
+//         last_name: 'Holt',
+//         avatar: 'https://reqres.in/img/faces/4-image.jpg'
+//       },
+//       {
+//         id: 5,
+//         email: 'charles.morris@reqres.in',
+//         first_name: 'Charles',
+//         last_name: 'Morris',
+//         avatar: 'https://reqres.in/img/faces/5-image.jpg'
+//       },
+//       {
+//         id: 6,
+//         email: 'tracey.ramos@reqres.in',
+//         first_name: 'Tracey',
+//         last_name: 'Ramos',
+//         avatar: 'https://reqres.in/img/faces/6-image.jpg'
+//       }
+//     ],
+//     support: {
+//       url: 'https://reqres.in/#support-heading',
+//       text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
+//     }
+//   }
