b9dad7869ee5c91eba02f7310a2e3fcbdb352317
Sharanya <datrangesharanya@gmail.com>
2024-02-27 15:11:50 +0530

    Asg8 formatting

--- Assignments/Asg8.js
+++ Assignments/Asg8.js
@@ -1,12 +1,6 @@
 //Question 1
-/* Assignments
-1. Write a function that can stop execution of a function for the number of milliseconds sent as an argument
-Example:
- const func = async () => {
-  console.log(“Printing before”)
-  //Call your function here eg. sleep(3000)
-  console.log(“Printing after”)
-}*/
+//1. Write a function that can stop execution of a function for the number of milliseconds sent as an argument
+
 function sleep(time) {
     return new Promise ((resolve) => setTimeout(resolve,time));
 }
@@ -21,11 +15,10 @@ const func = async () => {
     );
     //)};
 }
+//Printing before
 
 func();
-// output - Printing before
 
-console.log('2-----------------------------------------------------------------');
 //Question 2
 /* 2.  Using promises - write a function that fetches data from an API endpoint (GET https://reqres.in/api/users ). Log the data into the console once it is received*/
 function fetchData() {
@@ -35,19 +28,15 @@ function fetchData() {
         fetch(url)
 
         .then(response => {
-            //check response successful or not
             if(!response.ok) {
-                //if not - reject promise - error
                 reject(new Error("Failed to fetch data"));
             }
-            //successful - parse to json response
             return response.json();
     }) 
         .then (data => {
             resolve(data);
         })
         .catch(error => {
-            //reject promise of error occured
             reject(error);
         });
     });
@@ -61,158 +50,30 @@ fetchData()
         console.error("Error fetchong data: ", error);
     })
 
-//output - Data received:  {
-//   page: 1,
-//   per_page: 6,
-//   total: 12,
-//   total_pages: 2,
-//   data: [
-//     {
-//       id: 1,
-//       email: 'george.bluth@reqres.in',
-//       first_name: 'George',
-//       last_name: 'Bluth',
-//       avatar: 'https://reqres.in/img/faces/1-image.jpg'
-//     },
-//     {
-//       id: 2,
-//       email: 'janet.weaver@reqres.in',
-//       first_name: 'Janet',
-//       last_name: 'Weaver',
-//       avatar: 'https://reqres.in/img/faces/2-image.jpg'
-//     },
-//     {
-//       id: 3,
-//       email: 'emma.wong@reqres.in',
-//       first_name: 'Emma',
-//       last_name: 'Wong',
-//       avatar: 'https://reqres.in/img/faces/3-image.jpg'
-//     },
-//     {
-//       id: 4,
-//       email: 'eve.holt@reqres.in',
-//       first_name: 'Eve',
-//       last_name: 'Holt',
-//       avatar: 'https://reqres.in/img/faces/4-image.jpg'
-//     },
-//     {
-//       id: 5,
-//       email: 'charles.morris@reqres.in',
-//       first_name: 'Charles',
-//       last_name: 'Morris',
-//       avatar: 'https://reqres.in/img/faces/5-image.jpg'
-//     },
-//     {
-//       id: 6,
-//       email: 'tracey.ramos@reqres.in',
-//       first_name: 'Tracey',
-//       last_name: 'Ramos',
-//       avatar: 'https://reqres.in/img/faces/6-image.jpg'
-//     }
-//   ],
-//   support: {
-//     url: 'https://reqres.in/#support-heading',
-//     text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
-//   }
-// }
-console.log('3-----------------------------------------------------------------');
-
 //Question 3 -
-/* 
-3. What will be printed to the console when the promise resolves and when it rejects?
-const testAsyncFunction = () =>{
-return new Promise((resolve, reject) =>{
-if (Math.random() > 0.5) {
-resolve('Test Resolve');
-} else {
-reject('Test Reject');
-}
-}).catch((err) =>{
-console.log('Error caught in testAsyncFunction: ', err);
-});
+
+//3. What will be printed to the console when the promise resolves and when it rejects?
+
+
+const testAsyncFunction = () => {
+    return new Promise((resolve, reject) => {
+        if (Math.random() > 5) {
+            resolve('Test Resolve');
+        } else {
+            reject('Test Reject');
+        }
+    }).catch((err) => {
+        console.log('Error caught in testAsyncFunction: ', err);
+    });
 };
 testAsyncFunction()
-.then((res) =>{
-console.log('Response in then block: ', res);
-})
-.catch((err) => console.log('Error in catch block: ', err));
+    .then((res) => {
+        console.log('Response in then block: ', res);
+    })
+    .catch((err) => console.log('Error in catch block: ', err));
 
-
-*/
-
-const testAsyncFunction1 = () =>{
-    return new Promise((resolve, reject) =>{
-    if (10> 0.5) {
-    resolve('Test Resolve');
-    } else {
-    reject('Test Reject');
-    }
-    }).catch((err) =>{
-    console.log('Error caught in testAsyncFunction: ', err);
-    });
-    };
-testAsyncFunction1()
-.then((res) =>{
-console.log('Response in then block: ', res);
-})
-.catch((err) => console.log('Error in catch block: ', err));
-
-//output - 
-// Data received:  {
-//     page: 1,
-//     per_page: 6,
-//     total: 12,
-//     total_pages: 2,
-//     data: [
-//       {
-//         id: 1,
-//         email: 'george.bluth@reqres.in',
-//         first_name: 'George',
-//         last_name: 'Bluth',
-//         avatar: 'https://reqres.in/img/faces/1-image.jpg'
-//       },
-//       {
-//         id: 2,
-//         email: 'janet.weaver@reqres.in',
-//         first_name: 'Janet',
-//         last_name: 'Weaver',
-//         avatar: 'https://reqres.in/img/faces/2-image.jpg'
-//       },
-//       {
-//         id: 3,
-//         email: 'emma.wong@reqres.in',
-//         first_name: 'Emma',
-//         last_name: 'Wong',
-//         avatar: 'https://reqres.in/img/faces/3-image.jpg'
-//       },
-//       {
-//         id: 4,
-//         email: 'eve.holt@reqres.in',
-//         first_name: 'Eve',
-//         last_name: 'Holt',
-//         avatar: 'https://reqres.in/img/faces/4-image.jpg'
-//       },
-//       {
-//         id: 5,
-//         email: 'charles.morris@reqres.in',
-//         first_name: 'Charles',
-//         last_name: 'Morris',
-//         avatar: 'https://reqres.in/img/faces/5-image.jpg'
-//       },
-//       {
-//         id: 6,
-//         email: 'tracey.ramos@reqres.in',
-//         first_name: 'Tracey',
-//         last_name: 'Ramos',
-//         avatar: 'https://reqres.in/img/faces/6-image.jpg'
-//       }
-//     ],
-//     support: {
-//       url: 'https://reqres.in/#support-heading',
-//       text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
-//     }
-//   }
-console.log('4-----------------------------------------------------------------');
+// when promise resolves, it prints := Response in then block:  Test Resolve
+// when promise rejects, it prints := Error caught in testAsyncFunction:  Test Reject, Response in then block:  undefined 
 
 //Question 4 - what will be printed - 
 const testAsyncFunction2 = () =>{
@@ -226,30 +87,25 @@ console.log('Response in then block', res);
 //output - Error in catch block Test static reject
 
 
-console.log('5-----------------------------------------------------------------');
-
 //Question 5 - what will be printed
-const testAsyncFunction3 = () =>{
-    return new Promise((resolve, reject) =>{
-    if (Math.random() > 0.5) {
-    resolve('Test Resolve');
-    } else {
-    reject('Test Reject');
-    }
-    }).catch((err) =>{
-    console.log('Error caught in testAsyncFunction', err);
-    throw new Error('Forced error');
+const testAsyncFunction3 = () => {
+    return new Promise((resolve, reject) => {
+        if (Math.random() > 0.5) {
+            resolve('Test Resolve');
+        } else {
+            reject('Test Reject');
+        }
+    }).catch((err) => {
+        console.log('Error caught in testAsyncFunction', err);
+        throw new Error('Forced error');
     });
-    };
+};
 testAsyncFunction3()
-.then((res) =>{
-console.log('Response in then block: ', res);
-})
-.catch((err) => console.log('Error in catch block: ', err));
-//output - Response in then block:  Test Resolve
-
-
-console.log('6-----------------------------------------------------------------');
+    .then((res) => {
+        console.log('Response in then block: ', res);
+    })
+    .catch((err) => console.log('Error in catch block: ', err));
+// output := Error in catch block:  Error: Forced error
 
 //Question 6 
 /*
@@ -279,7 +135,6 @@ function fetchData() {
 
 fetchData() 
 
-console.log('7-----------------------------------------------------------------');
 
 //Question 7 - Complete the above tasks with async/await.
 async function fetchDataAsync() {
@@ -307,59 +162,3 @@ async function fetchDataAndLog() {
     }
 }
 fetchDataAndLog() ;
-
-//output - 
-// Data recieved after delay  {
-//     page: 1,
-//     per_page: 6,
-//     total: 12,
-//     total_pages: 2,
-//     data: [
-//       {
-//         id: 1,
-//         email: 'george.bluth@reqres.in',
-//         first_name: 'George',
-//         last_name: 'Bluth',
-//         avatar: 'https://reqres.in/img/faces/1-image.jpg'
-//       },
-//       {
-//         id: 2,
-//         email: 'janet.weaver@reqres.in',
-//         first_name: 'Janet',
-//         last_name: 'Weaver',
-//         avatar: 'https://reqres.in/img/faces/2-image.jpg'
-//       },
-//       {
-//         id: 3,
-//         email: 'emma.wong@reqres.in',
-//         first_name: 'Emma',
-//         last_name: 'Wong',
-//         avatar: 'https://reqres.in/img/faces/3-image.jpg'
-//       },
-//       {
-//         id: 4,
-//         email: 'eve.holt@reqres.in',
-//         first_name: 'Eve',
-//         last_name: 'Holt',
-//         avatar: 'https://reqres.in/img/faces/4-image.jpg'
-//       },
-//       {
-//         id: 5,
-//         email: 'charles.morris@reqres.in',
-//         first_name: 'Charles',
-//         last_name: 'Morris',
-//         avatar: 'https://reqres.in/img/faces/5-image.jpg'
-//       },
-//       {
-//         id: 6,
-//         email: 'tracey.ramos@reqres.in',
-//         first_name: 'Tracey',
-//         last_name: 'Ramos',
-//         avatar: 'https://reqres.in/img/faces/6-image.jpg'
-//       }
-//     ],
-//     support: {
-//       url: 'https://reqres.in/#support-heading',
-//       text: 'To keep ReqRes free, contributions towards server costs are appreciated!'
-//     }
-//   }
